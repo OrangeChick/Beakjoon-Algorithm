45100b0c0b6264ea5c84b37fe0c65b8ec7437bab
Ji Yoon Park (JUNY) <99635366+OrangeChick@users.noreply.github.com>
2022-08-21 01:13:04 +0900

    [Silver V] Title: 나이순 정렬, Time: 376 ms, Memory: 26096 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
@@ -0,0 +1,6 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
+const N = input.shift();
+
+input.sort((a, b) => parseFloat(a) - parseFloat(b));
+
+console.log(input.join("\n"));
