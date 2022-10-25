f8c928e085ceb816e5f5e990a88e48f6ea23c6f5
Ji Yoon Park (JUNY) <99635366+OrangeChick@users.noreply.github.com>
2022-10-25 09:34:51 +0900

    [Bronze V] Title: A+B - 5, Time: 132 ms, Memory: 9568 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.js
@@ -0,0 +1,17 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let numbers = [];
+
+for (let i = 0; i < input.length - 1; i++) {
+  if (input[i] !== '') {
+    numbers.push(input[i].split(' '));
+  }
+}
+
+for (let i = 0; i < numbers.length - 1; i++){
+  let num1 = Number(numbers[i][0]);
+  let num2 = Number(numbers[i][1]);
+
+  console.log(num1 + num2);
+}
